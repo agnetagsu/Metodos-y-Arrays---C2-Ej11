0eaa81d972639dc33612841ff4a70de2c01e50a5
Agneta <agnetagsu@yahoo.es>
2021-05-23 12:12:44 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] miarray = new int[10];
+            int misuma = 0;
+
+            for (int i = 0; i < miarray.Length; i++)
+            {
+                miarray[i] = i;
+                misuma = misuma + i;
+            }
+          
+            Console.WriteLine(misuma);
+            
+        }
+    }
+}
